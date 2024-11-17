0ec2a2b2b8479065e5703eca0c6d208b3e0ff16c
bighux2 <49080823+bighux2@users.noreply.github.com>
2024-11-17 13:57:21 +0100

    Dev beewi

--- src/devices/BEEWIBBW200_json.h
+++ src/devices/BEEWIBBW200_json.h
@@ -1,6 +1,6 @@
 #include "common_props.h"
 
-const char* _BEEWIBBW200_json = "{\"brand\":\"BeeWi\",\"model\":\"BBW200\",\"model_id\":\"BEEWI/BBW200\",\"tag\":\"01\",\"condition\":[\"manufacturerdata\",\"index\",0,\"0d0005\",\"|\",\"name\",\"=\",\"beewibbw200\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",7,4,false,true,true],\"post_proc\":[\"/\",10]},\"hum\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",10,2,false,false,false]},\"batt\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",15,2,false,false,false]}}}";
+const char* _BEEWIBBW200_json = "";
 /* R""""(
 {
    "brand":"BeeWi",
@@ -10,16 +10,16 @@ const char* _BEEWIBBW200_json = "{\"brand\":\"BeeWi\",\"model\":\"BBW200\",\"mod
    "condition":["manufacturerdata", "index", 0 , "0d0005", "|", "name", "=", "beewibbw200"],
    "properties":{
       "tempc":{
-         "decoder":["value_from_hex_data", "manufacturerdata", 7, 4, false, true, true],
-         "post_proc":["/", 10]
+         "decoder":["value_from_hex_data", "manufacturerdata", 8, 4, true, true, false],
+         "post_proc":["/", 10.0]
       },
       "hum":{
-         "decoder":["value_from_hex_data", "manufacturerdata", 10, 2, false, false, false]
+         "decoder":["value_from_hex_data", "manufacturerdata", 14, 2, false, false, false]
       },
       "batt":{
-         "decoder":["value_from_hex_data", "manufacturerdata", 15, 2, false, false, false]
+         "decoder":["value_from_hex_data", "manufacturerdata", 24, 2, false, false, false]
       }
    }
 })"""";*/
 
-const char* _BEEWIBBW200_json_props = _common_BVTH_props;
+const char* _BEEWIBBW200_json_props = _common_BTH_props;
